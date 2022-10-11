f0aa2865fecf28c32218b1ed8b75d16bf383b872
Benjamin Martinez <benjaminmartinez.sia@gmail.com>
2022-10-11 15:39:18 -0400

    Initial commit

---
+++ JenkinsHello/src/main/java/com/hellojenkins/HelloJenkins.java
@@ -0,0 +1,16 @@
+package com.hellojenkins;
+
+public class HelloJenkins {
+
+	public static void main(String[] args) {
+		System.out.println(helloJenkins());
+		System.out.println(helloAgainJenkins());
+	}
+
+	public static String helloJenkins() {
+		return "hello jenkins";
+	}
+	public static String helloAgainJenkins() {
+		return "hello again jenkins";
+	}
+}
